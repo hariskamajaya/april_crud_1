9379c512a799ed29368250e371b7d551cfbedd0d
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-04-23 14:14:56 +0700

    3.14

--- resources/views/buku/create.blade.php
+++ resources/views/buku/create.blade.php
@@ -10,10 +10,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <h4 class="px-6 py-2 text-gray-900 dark:text-gray-100">
+                <h4 class="p-6 text-gray-900 dark:text-gray-100">
                     Masukan data buku baru
                 </h4>
-                <div class="overflow-x-auto px-6 py-6">
+                <div class="overflow-x-auto p-6">
                     <form action="{{route('buku.post')}}" method="post">
                         @csrf
                         <div class="mt-4">
@@ -21,6 +21,7 @@
                             <x-text-input id="judul_buku" class="block mt-1 w-full" type="text" name="judul_buku" :value="old('judul_buku')" required />
                             <x-input-error :messages="$errors->get('judul_buku')" class="mt-2" />
                         </div>
+
                         <div class="mt-4">
                             <x-input-label for="id_lemari" :value="__('Lemari')" />
                             {{-- <x-text-input id="judul_buku" class="block mt-1 w-full" type="text" name="judul_buku" :value="old('judul_buku')" required /> --}}
@@ -29,26 +30,28 @@
                                 @foreach ($lemari as $item)
                                     <option value="{{$item->id}}">{{$item->nama_lemari}}</option>
                                 @endforeach
-
                             </select>
-
                             <x-input-error :messages="$errors->get('id_lemari')" class="mt-2" />
                         </div>
+
                         <div class="mt-4">
                             <x-input-label for="penulis" :value="__('Penulis')" />
                             <x-text-input id="penulis" class="block mt-1 w-full" type="text" name="penulis" :value="old('penulis')" required />
                             <x-input-error :messages="$errors->get('penulis')" class="mt-2" />
                         </div>
+
                         <div class="mt-4">
                             <x-input-label for="penerbit" :value="__('Penerbit')" />
                             <x-text-input id="penerbit" class="block mt-1 w-full" type="text" name="penerbit" :value="old('penerbit')" required />
                             <x-input-error :messages="$errors->get('penerbit')" class="mt-2" />
                         </div>
+
                         <div class="mt-4">
                             <x-input-label for="cover" :value="__('Cover')" />
                             <x-text-input id="cover" class="block mt-1 w-full" type="file" name="cover" :value="old('cover')" required />
                             <x-input-error :messages="$errors->get('cover')" class="mt-2" />
                         </div>
+
                         <div class="mt-4">
                             <button class="bg-red-600 hover:bg-red-800 text-white px-6 py-2 rounded-md">Tambah</button>
                         </div>
